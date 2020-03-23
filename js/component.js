26d42c25d6febf0dc008dc7e4f55711fc0d34fd6
David Forero <davidforero717@gmail.com>
2020-03-22 21:23:16 -0500

    final de componentes

--- js/component.js
+++ js/component.js
@@ -1,11 +1,15 @@
 Vue.component('mi-primer-componente',{
-    template:<div class="alert alert-primary" role="alert">
-    This is a primary alert—check it out!
-  </div>
+    props:['msg'],
+    template:'<div class="alert alert-primary" role="alert"><button type="button" class="btn btn-info" style="margin:10px" v-on:click="saludar(msg)">click</button>{{msg}}</div>',
+    methods:{
+        saludar:function(valor) {
+            alert("Hola " + valor)
+        }
+    }
 });
 
-var app = new Vue({
-    el:"app",
+var app1 = new Vue({
+    el:"#app",
     data:{
      
     }
